5701d245454a46e12e05c50d5975c25597232573
Dana Bradley <dbradley0909@outlook.com>
2022-01-10 22:56:09 -0500

    Initial commit

---
+++ src/main/java/com/devmountain/tutorial/spring/SpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.tutorial.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApplication {
+// constructor
+	public static void main(String[] args) {SpringApplication.run(SpringTutorialApplication.class, args);
+		System.out.println("wassup pluralsight!");
+	}
+
+}
---
+++ src/test/java/com/devmountain/tutorial/spring/SpringTutorialApplicationTests.java
@@ -0,0 +1,14 @@
+package com.devmountain.tutorial.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringRuner.class)
+@SpringBootTest
+public class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
